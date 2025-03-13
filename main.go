b49c468e02b2321cb0ddfc33d37e9116056c18f5
KostianDev <verbytskyi.kostiantyn@lll.kpi.ua>
2025-03-13 20:38:11 +0200

    Add main module to control modes and user I/O

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"math"
+	"lab1/solver"
+	"lab1/interactive"
+	"lab1/noninteractive"
+)
+
+func main() {
+	var a, b, c float64
+	var x1, x2 float64
+
+	switch len(os.Args) {
+	case 1:
+		a, b, c = interactive.RunInteractiveMode()
+	case 2:
+		a, b, c = noninteractive.RunNonInteractiveMode(os.Args[1])
+	default:
+		fmt.Println("Error: invalid number of arguments")
+		os.Exit(1)
+	}
+
+	if math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) {
+		os.Exit(1)
+	}
+
+	fmt.Printf("The equation is: (%g)x^2 + (%g)x + (%g) = 0\n", a, b, c)
+
+	x1, x2 = solver.SolveQuadEquation(a, b, c)
+	if math.IsNaN(x1) || math.IsNaN(x2) {
+		fmt.Println("Error: Equation has no real roots")
+		os.Exit(1)
+	} else if x1 == x2 {
+		fmt.Printf("There is only one root:\nx = %f\n", x1)
+	} else {
+		fmt.Printf("The roots are:\nx1 = %f, x2 = %f\n", x1, x2)
+	}
+
+	os.Exit(0)
+}
